fa9736fb108d65d5ea43ec8010c847a5a6fc667d
sajjad-mugdho <sajjadmugdho@yahoo.com>
2023-02-19 03:31:59 +0600

    vercel deploy fixed

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -24,7 +24,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
     const patchFriend = async () => {
         const response = await fetch(
-            `http://localhost:3001/users/${_id}/${friendId}`,
+            `https://facemimu2-sajjad-mugdho.vercel.app/users/${_id}/${friendId}`,
             {
                 method: "PATCH",
                 headers: {
